6535413e649f1828ea86c9cdf4769b4944f513be
shash <shashvat.bharti@gmail.com>
2021-06-17 21:14:46 +0530

    converting data to dataframe

--- data_processing/processor.py
+++ data_processing/processor.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+from bs4 import BeautifulSoup
+import pandas as pd
 
 
 def get_data_path(dataset_name):
@@ -24,11 +26,12 @@ def get_file_list(data_path, year, dataset_name):
         elif year == '2003':
             indices = list(range(1, 51))
             for i in indices:
-                temp = [j for j in file_list if j.startswith('N' + str(i))]
+                temp = [j for j in file_list if j.startswith('N' + str(i) + '.')]
                 assert len(temp) == 1
                 files.append(temp[0])
         else:
             raise Exception('Invalid Year')
+        files = [os.path.join(data_path, i) for i in files]
     # TODO RTE data
     return files
 
@@ -39,12 +42,12 @@ def get_metadata(data_path, dataset_name, year, novel=False):
             if novel:
                 file_name = os.path.join(data_path, 'novel_sentences_2004.txt')
             else:
-                file_name = os.path.join(data_path, 'relavant_sentences_2004.txt')
+                file_name = os.path.join(data_path, 'relevant_sentences_2004.txt')
         if year == '2003':
             if novel:
                 file_name = os.path.join(data_path, 'novel_sentences_2003.txt')
             else:
-                file_name = os.path.join(data_path, 'relavant_sentences_2003.txt')
+                file_name = os.path.join(data_path, 'relevant_sentences_2003.txt')
         with open(file_name, 'r') as f:
             content = f.readlines()
         metadata = []
@@ -56,9 +59,37 @@ def get_metadata(data_path, dataset_name, year, novel=False):
     # TODO RTE
 
 
+def convert_data_to_df(file_list, dataset, year):
+    df = pd.DataFrame(columns=['text', 'docid', 'line_number', 'file'])
+    for file in file_list:
+        with open(file, 'r') as f:
+            data = f.read()
+
+        if dataset == 'trec':
+            if year == '2004':
+                file = 'N' + file.split('\\')[-1].split('.')[0]
+            elif year == '2003':
+                file = file.split('\\')[-1].split('.')[0]
+        # TODO RTE
+
+        data = BeautifulSoup(data, "lxml")
+        data = data.find_all('s')
+        for i in data:
+            df2 = {'text': i.text, 'docid': i.get('docid'), 'line_number': i.get('num'), 'file': file}
+            df = df.append(df2, ignore_index=True)
+    return df
+
+
+def add_flag(df, metadata, flag_column):
+    df[flag_column] = False
+    for i in metadata:
+        df.loc[(df["file"] == i[0]) & (df["docid"] == i[1]) & (df["line_number"] == i[2]), flag_column] = True
+    return df
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--year', default='2004')
+    parser.add_argument('--year', default='2003')
     parser.add_argument('--dataset', default='trec')
 
     args = parser.parse_args()
@@ -76,3 +107,13 @@ if __name__ == '__main__':
     relevant_data_meta = get_metadata(data_path, dataset_name, year)
     novel_data_meta = get_metadata(data_path, dataset_name, year, novel=True)
 
+    # Convert data to pandas dataframe
+    df = convert_data_to_df(file_list, dataset_name, year)
+
+    # Add flags for relevant and novel data
+    df = add_flag(df, relevant_data_meta, 'relevant_flag')
+    df = add_flag(df, novel_data_meta, 'novel_flag')
+
+    # Export data
+    df = df.drop_duplicates()
+    df.to_parquet(os.path.join(data_path, year + '.parquet'))
