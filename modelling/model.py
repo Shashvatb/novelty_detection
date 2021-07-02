3e3cae35264b0070ffdb4243be154f62dbd63ea1
shash <shashvat.bharti@gmail.com>
2021-07-02 23:58:53 +0530

    completed model

--- modelling/model.py
+++ modelling/model.py
@@ -1,16 +1,31 @@
 from gensim.models import Word2Vec
+from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+import pickle
+import os
 
 
 class MemNet(object):
-    def __init__(self):
-        self.I = Word2Vec(vector_size=100, window=3, min_count=1, workers=-1)
+    def __init__(self, path, train=None):
+        self.path = path
+        if not train:
+            with open(os.path.join(path, 'vectorizer.pkl'), 'rb') as f:
+                self.I = pickle.load(f)
+            with open(os.path.join(path, 'memory.pkl'), 'rb') as f:
+                self.M = pickle.load(f)
+
+        else:
+            self.M = None
+            self.I = Word2Vec(vector_size=100, window=3, min_count=1, workers=-1)
+        self.G = cosine_similarity
 
     def input_layer_train(self, x):
         x = [i.split() for i in x]
         self.I.build_vocab(x)
         total_examples = self.I.corpus_count
         self.I.train(x, total_examples=total_examples, epochs=self.I.epochs)
+        with open(os.path.join(self.path, 'vectorizer.pkl'), 'wb') as f:
+            pickle.dump(self.I, f)
         print('model trained')
 
     def input_layer(self, x):
@@ -25,7 +40,7 @@ class MemNet(object):
                         sentence.append(self.I.wv[j])
                     except KeyError:
                         pass
-                y_hat.append(np.mean(sentence, axis=1))
+                y_hat.append(np.mean(sentence, axis=0))
         elif type(x) == str:
             words = x.split()
             sentence = []
@@ -34,7 +49,41 @@ class MemNet(object):
                     sentence.append(self.I.wv[j])
                 except KeyError:
                     pass
-            y_hat.append(np.mean(sentence, axis=1))
+            y_hat.append(np.mean(sentence, axis=0))
         else:
             print(type(x))
         return y_hat
+
+    def memory_unit_train(self, x):
+        vectors = self.input_layer(x)
+
+        assert len(vectors[0]) == len(vectors[1])
+        
+        with open(os.path.join(self.path, 'memory.pkl'), 'wb') as f:
+            pickle.dump(vectors, f)
+
+    def memory_unit(self, x):
+        if type(x) == list or type(x) == np.ndarray or type(x) == str:
+            similarities = []
+            if type(x) == str:
+                x = [x]
+            for i in x:
+                best = 0.0
+                for j in self.M:
+                    temp = self.G(j, i)
+                    if temp > best:
+                        best = temp
+                similarities.append(best)
+            return similarities
+        else:
+            print(type(x))
+            return None
+
+    def inf(self, x, threshold):
+        vals = []
+        for i in x:
+            if i >= threshold:
+                vals.append(True)
+            else:
+                vals.append(False)
+        return vals
--- modelling/train.py
+++ modelling/train.py
@@ -2,10 +2,11 @@ import argparse
 import os
 import sys
 import numpy as np
+import pandas as pd
 
 sys.path.insert(0, os.getcwd())
 from modelling.model import MemNet
-from data_processing.data_preprocess import load_data
+from data_processing.data_preprocess import get_data_path
 from config import text_column
 
 
@@ -14,18 +15,24 @@ def get_sentences(df):
     return sentences
 
 
+def get_train_data(path):
+    df = pd.read_parquet(os.path.join(path, 'train.parquet'))
+    return df
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--year', default='2004')
     parser.add_argument('--dataset', default='trec')
     args = parser.parse_args()
 
-    year = args.year + '_preprocessed'
     dataset_name = args.dataset
 
-    df = load_data(dataset_name, year)
-    sentences = get_sentences(df)
+    path = get_data_path(dataset_name)
 
-    model = MemNet()
+    train_data = get_train_data(path)
+    sentences = get_sentences(train_data)
+
+    model = MemNet(path, train=True)
     model.input_layer_train(sentences)
-    print(model.input_layer(np.array(sentences[:2])))
+
+    model.memory_unit_train(sentences)
