7db71f0316355ae3f9b55246ee1129c530a6a923
shash <shashvat.bharti@gmail.com>
2021-07-05 22:05:21 +0530

    added print statements for number of sentences in train

--- modelling/train.py
+++ modelling/train.py
@@ -33,9 +33,11 @@ if __name__ == '__main__':
 
     train_data = get_train_data(path)
     sentences = get_sentences(train_data)
+    print("number of sentences for training the vectorizer: ", len(sentences))
 
     model = MemNet(path, train=True)
     model.input_layer_train(sentences)
 
     sentences = get_sentences(train_data, novel_flag, val=True)
     model.memory_unit_train(sentences)
+    print("number of sentences for memory unit: ", len(sentences))
