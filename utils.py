f37a8c290bd7cd224921cbfd65034e575b21f5a6
shash <shashvat.bharti@gmail.com>
2021-09-07 21:14:01 +0530

    printing hidden layers dim

--- utils.py
+++ utils.py
@@ -28,7 +28,7 @@ def gen_observations(data, tokenizer, model):
         outputs = model(**inputs, labels=labels[i], output_hidden_states=True)
         outputs = outputs.hidden_states
         if i == 0:
-            print(outputs[-1].size())
+            print(outputs[-1].mean(0).size())
         result += _shared(outputs[0].cpu().detach().numpy())
 
     assert len(result) == len(labels)
